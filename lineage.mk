65b42613c54fa590c650bd54e2a7b7636d9227b9
arttttt <artem-bambalov@yandex.ru>
2017-12-03 03:51:00 +0300

    mocha: CM vendor renamed to lineage

--- lineage.mk
+++ lineage.mk
@@ -5,8 +5,8 @@ $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
